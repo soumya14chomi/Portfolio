62789f624c71b1ef277bf28e1bf6edf6b1ccfc4b
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2024-01-25 12:25:25 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,267 @@
+export const Bio = {
+  name: "Soumya Namani",
+  roles: [
+    "Software Developer",
+  ],
+  description: "Passionate Software Engineer dedicated to continuous learning, embracing challenges, and committed to hard work for innovative solutions.",
+  github: "https://github.com/soumya14chomi",
+  resume:
+    "https://drive.google.com/file/d/15JVsDATiVhRQwGjU60Cw-L4_sDPJ2FXU/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/soumya-namani-750323132/",
+  mail: "soumya.chomi.1998@gmail.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "PostgreSQL",
+        image: "https://miro.medium.com/v2/resize:fit:828/format:webp/1*jk5ExetrwM-G_XCiEgUTKQ.png",
+      },
+      {
+        name: "Oracle PL/SQL",
+        image: "https://www.oracle.com/a/ocom/img/pl-sql.svg"
+      },
+      {
+        name: "Microsoft SQL Server",
+        image : "https://tribes.agency/wp-content/uploads/2023/10/ext-550.png",
+      },
+
+    ],
+  },
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name:  "Java",
+        image : "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/800px-Java_programming_language_logo.svg.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/sql/sql.png?size=48",
+      },
+      {
+        name: "C",
+        image:
+          "https://raw.githubusercontent.com/github/explore/f3e22f0dca2be955676bc70d6214b95b13354ee8/topics/c/c.png?size=48",
+      },
+      {
+        name: "C++",
+        image : "https://w7.pngwing.com/pngs/46/626/png-transparent-c-logo-the-c-programming-language-computer-icons-computer-programming-source-code-programming-miscellaneous-template-blue.png",
+      },
+      {
+        name: "Matlab",
+        image:
+          "https://raw.githubusercontent.com/github/explore/fb1413905cbb7f6639f234c4e2c933e69f484a4f/topics/matlab/matlab.png?size=48",
+      },
+
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Data Structures and ALgorithms",
+        image : "https://w7.pngwing.com/pngs/580/809/png-transparent-data-structure-logo-brand-data-structure-blue-text-logo-thumbnail.png",
+      },
+      {
+        name: "Object-Oriented Programming",
+        image : "https://cdn.vectorstock.com/i/1000x1000/82/29/object-oriented-programming-isolated-icon-simple-vector-28218229.webp",
+      },
+      {
+        name: "Web Application Development",
+        image: "https://img.freepik.com/premium-vector/web-developer-logo-template_674356-207.jpg?w=740",
+      },
+      {
+        name : "Spring",
+        image : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Spring_Boot.svg/180px-Spring_Boot.svg.png",
+      },
+      {
+        name:"Eclipse",
+        image : "https://w7.pngwing.com/pngs/95/978/png-transparent-eclipse-foundation-scalable-graphics-jetty-eclipse-purple-blue-violet-thumbnail.png",
+      },
+      {
+        name: "Netbeans",
+        image :"https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Apache_NetBeans_Logo.svg/180px-Apache_NetBeans_Logo.svg.png",
+      },
+      {
+        name: "Visual Studio Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Unit Testing",
+        image: "https://static-00.iconduck.com/assets.00/unit-testing-icon-2048x2048-8bw7nxcv.png",
+      },
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "GitLab",
+        image:
+          "https://github.com/gitlabhq.png?size=40",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.webopedia.com/wp-content/uploads/1997/02/Webo.OracleProfile-300x300.png",
+    role: "Associate Software Developer",
+    company: "Oracle India",
+    date: "Aug 2021 - Aug 2022",
+    desc: "•Led frontend and backend development for NetSuite ERP Applications, significantly improving user experience and system efficiency.\n• Played a pivotal role in design and developing an Oracle application using TypeScript, SuiteScript, Java, and Oracle SQL, streamlining organizational hierarchy and payroll management for increased efficiency. \n ",
+    skills: [
+      "JavaScript", "TypeScript", "Java", "Node.js", "Oracle PL/SQL", "Visual Studio", "Eclipse", "Git", "Application Developement", 
+    ],
+  },
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQF8MuW_mn_EOA/company-logo_200_200/0/1675686951203/fusion_technologies_services_logo?e=2147483647&v=beta&t=hYAcv6cbPzRTq-T30UmfCoa0_PX8mdDe2hSpueEwnhQ",
+    role: "Python Developer Intern",
+    company: "Fusion Technologies",
+    date: "June 2020 - July 2021",
+    desc: "• Executed end-to-end development of a web application, seamlessly integrating front-end and back-end functionalities utilizing diverse Python modules and conducted a performance analysis of the code and enhanced the efficiency of the Python implementation through optimization. • Implemented a global queue for task distribution among threads and integrated a thread pool using into server code, demonstrating effective task management skills, producing data for thread consumption, highlighting attention to system responsiveness. ",
+    skills: [
+      "Python", 
+      "tkinter", 
+      "Shell Scripting",
+      "Email Monitoring",
+      "JSON",
+      "Multi-threading",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEegGtNEUd_a9hLukKVgzKQKaHsuaLeA79mA&usqp=CAU",
+    school: "Pennsylvania State University",
+    date: "Aug 2022 - Present",
+    grade: "3.7 GPA",
+    desc: "I am currently pursuing my second masters in Computers and Information Sciences. I have taken courses in Data Warehousing, Advance DataBase Management Systems, Design and Analysis of Algorithms, Computer Security, Natural Language Processing and Principles of Programming Languages.",
+    degree: "Master's of Science - M.S., Computers and Information Sciences",
+  },
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/en/e/ea/JNTU_Hyderabad_logo.png",
+    school: "Jawaharlal Nehru Technological University Hyderabad",
+    date: "Jan 2020 - Aug 2021",
+    grade: "3.9 GPA",
+    desc: "I have completed my masters in Computer Science with GPA 3.9, in which I have taken the courses Big Data Analytics, Mobile Application Development, High Performance Computing and completed a project.",
+    degree: "Master's of Technology - M.Tech, Computer Science",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/en/e/ea/JNTU_Hyderabad_logo.png",
+    school: "Jawaharlal Nehru Technological University Hyderabad",
+    date: "Aug 2016 - Apr 2020",
+    grade: "3.7 GPA",
+    desc: "I have completed my Bachelors in COmputer Science and Information Science with 3.7 GPA, where I have taken the courses Object Oriented Programming, Data Structures and Algorithms, Web Technologies, Grid and Cloud Computing, Computer Networks, Artificial Intelligence and Machine Learning.",
+    degree: "Bachelors of Technology, B.Tech, Computer Science and Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Theft Detection System",
+    description:
+      "An advanced monitoring and alarm mechanism utilizing a CNN trained on a specific dataset to identify potential weapons and individuals who may pose a risk of committing criminal activities based on their facial features. This real-time system notifies owners of possible threats, boasting a 96% accuracy rate across a dataset of 1,500 images.       ",
+    image:
+    "https://m.media-amazon.com/images/I/711rLZzlEDL.jpg",
+    tags: [
+      "Python",
+      "Machine Learning",  
+      "Model Training",
+      "Image Processing - OpenCV",
+      "Facial Recognition - Haar Cascade",
+      "Email - smtplib module",
+      "Socket.IO",
+    ],
+    category: "ML Projects",
+    github: "https://github.com/soumya14chomi/theft-detection-system/tree/main/TheftDetection/model",
+    webapp: "https://github.com/soumya14chomi/theft-detection-system/tree/main/TheftDetection/model",
+  },
+  {
+    id: 0,
+    title: " Electricity Price Forecasting by Combining Bidirectional LSTMs With Autoencoders",
+    description:
+    "Innovative application of advanced deep learning techniques for enhanced accuracy in electricity price forecasting, demonstrated through superior model performance metrics",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBcR0JM2icIpd7-wVDPyNfWUCJDbOATtWQcQ&usqp=CAU",
+    tags: ["Python", "Autoencoders", "Model Training", "Preprocessing", "Datacleaning", "Git", "VS Code",],
+    category: "ML Projects",
+    github: "https://github.com/soumya14chomi/Electricty-price-forecasting/blob/main/Autoencoders.ipynb",
+    webapp: "https://github.com/soumya14chomi/Electricty-price-forecasting/blob/main/Autoencoders.ipynb",
+  },
+  {
+    id: 1,
+    title: "Driver Drowsiness Detection",
+    description:
+      " An intelligent alert system that signals drivers if they begin to fall asleep at the wheel. The model demonstrated a 99% accuracy, having been trained on a dataset comprising 10,000 images.",
+    image:
+      "https://www.astiinfotech.com/assets/blogAssets/images/DDD-2.jpg",
+    tags: [
+      "HTML", "CSS", "JavaScript", "React.js", "Git", "VS Code",
+    ],
+    category: "ML Projects",
+    github: "https://github.com/Singh-Supreet/Project_Kanban_board",
+    webapp: "https://singh-supreet.github.io/Project_Kanban_board/",
+  },
+];
